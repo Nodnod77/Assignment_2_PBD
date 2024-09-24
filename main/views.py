6239d4476997ff1963781a8d00e5a3f3efc2e920
Nodnod77 <donia.sakji@etu.imt-nord-europe.fr>
2024-09-24 22:38:53 +0700

    devoir 4

--- main/views.py
+++ main/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render, redirect
 from main.forms import FormEntryForm
 from main.models import FormEntry
-from django.http import HttpResponse
+from django.http import HttpResponse ,HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
+from django.contrib.auth import authenticate, login , logout
+from django.contrib import messages 
 from django.core import serializers
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+import datetime
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
     form_entries = FormEntry.objects.all()
 
     context = {
         'npm' : '2406394906 ',
         'name': 'Donia Sakji',
+        'login name' : request.user.username,
         'class': 'PBP KKI',
         'appName': 'nodnodShop',
         'form_entries': form_entries,
+        'last_login': request.COOKIES.get('last_login', 'Never') ,
     }
 
     return render(request, "main.html", context)
@@ -45,3 +54,38 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = FormEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+##login lgout
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
